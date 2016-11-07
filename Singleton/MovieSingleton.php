0aeaa273f4a3a0a0e0c7b6728c86972c2b443bcd
LindseyArmour <la95@njit.edu>
2016-11-07 02:08:09 -0500

    fixed errors

--- Singleton/MovieSingleton.php
+++ Singleton/MovieSingleton.php
@@ -1,9 +1,9 @@
 <?php
 //singleton pattern a class can distribute one instance of itself to other classes
 class MovieSingleton {
-  private $director = Mike Mitchell
-  private $movieName = Trolls
-  private $castStar = Justin Timberlake
+  private $director = ' Mike Mitchell';
+  private $movieName = ' Trolls';
+  private $castStar = 'Justin Timberlake';
   // hold the class instance. static property to mediate object instantiation
   private static $movie = NULL;
   private static $movieRentedOut = FALSE;
@@ -32,19 +32,11 @@ class MovieSingleton {
  
  function getMovieName() {return $this->movieName;}
  
- function getcastStar() {return $this=>castStar;}
+ function getcastStar() {return $this->castStar;}
  
  function getDirectorandMovieNameandcastStar() {
    return $this->getMovieName() . ' by ' . $this->getDirector() . ' starring ' . $this->getcastStar();
  }
-}
-
-class MovieRenter() {
-   private $movieRented;
-   private $movieavailable = FALSE;
-   
-   function __constructor() {
-   
-   
+}   
   
 ?>
