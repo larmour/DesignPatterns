bfe26627359d30aa3d08c7f428bb747eee3e1dbf
LindseyArmour <la95@njit.edu>
2016-11-08 12:54:55 -0500

    added a line

--- Strategy/strategytest.php
+++ Strategy/strategytest.php
@@ -8,6 +8,7 @@ echo "Testing out 2.. \n";
 $commander = new Commander('2');
 $commander->command();
 echo "<br>";
+echo "Testing out 3.. \n";
 $commander = new Commander('3');
 $commander->command();
 echo "<br>";
