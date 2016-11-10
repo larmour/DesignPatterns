04b36f3ae32b26c5d7f0eb28b37e6e506d05668c
LindseyArmour <la95@njit.edu>
2016-11-10 18:03:18 -0500

    form for program4

---
+++ program4/form.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title> Program 4 Form </title>
+</head>
+
+<form action="program4.php" method="post">
+ 
+ Choose a color to dye your hair: <br>
+ <input type="text" name="color"><br>
+ <input type="submit" value="Submit Please">
+</form>
+</html>
+
